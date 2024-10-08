c228a7249fa4fb11b60c951bf0f6a693e5855152
LD-2BAT <louiecamarines21@gmail.com>
2024-10-08 20:09:08 +0800

    Finished writing markup with JSX section

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
 function App() {
 
   return (
     <>
-    <MyButton />
+     <AboutPage/>
     </>
   )
 } 
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
 
 
 export default App
