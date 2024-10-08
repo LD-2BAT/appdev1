adf465cedf20544d41c10a12f0e6de853017ede8
LD-2BAT <louiecamarines21@gmail.com>
2024-10-08 20:03:47 +0800

    Finished creating and nesting component section

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+    <MyButton />
+    </>
+  )
+} 
+
+
+export default App
