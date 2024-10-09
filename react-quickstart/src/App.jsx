019f051018b5472d3e9664732870dbddc375fa41
LD-2BAT <louiecamarines21@gmail.com>
2024-10-09 08:18:54 +0800

    Finished Using Hooks and Sharing Data between Components section

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -5,30 +5,31 @@ import { useState } from 'react';
 
 
 function App() {
-
-  return (
-  <>
-  <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  </>
-  )
-}
-function MyButton() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
+ 
 
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
+  <>
+ <div>
+  	<h1>Counters that update together</h1>
+  	<MyButton count={count} onClick={handleClick} />
+  	<MyButton count={count} onClick={handleClick} />
+	</div>
+  </>
+  )
+}
+function MyButton({ count, onClick }) {
+  return (
+	<button onClick={onClick}>
+  	Clicked {count} times
+	</button>
   );
 }
+
 
 
 
